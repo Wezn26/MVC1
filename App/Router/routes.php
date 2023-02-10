ea7de9b11483e0fff7192d93a8f759cb33821dc6
Leonid Second Comp <li_che_in@mail.ru>
2023-02-10 16:29:47 +0300

    Create: App\Controllers\Controller.php, App\Models\Model.php, App\View\View.php Add code in App\Router\routes.php

--- App/Router/routes.php
+++ App/Router/routes.php
@@ -1,4 +1,27 @@
 <?php
 return [
+   '' => [
+       'controller' => 'main',
+       'action' => 'index'
+   ],
     
+   'contacts' => [
+       'controller' => 'contacts',
+       'action' => 'contact'
+   ],
+   
+   'account/login' => [
+       'controller' => 'account',
+       'action' => 'login'
+   ],
+   
+   'account/register' => [
+       'controller' => 'account',
+       'action' => 'register'
+   ],
+   
+   'news/show' => [
+       'controller' => 'news',
+       'action' => 'show'
+   ], 
 ];
