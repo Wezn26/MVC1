cc600a0d131745278f014870b4f1d0ff4e6c14ba
Leonid Second Comp <li_che_in@mail.ru>
2023-02-10 17:13:52 +0300

    Start develop function view()

--- App/View/View.php
+++ App/View/View.php
@@ -3,8 +3,25 @@ namespace App\View;
 
 class View
 {
+    public $path;
+    public $route;
 
     public function __construct()
-    {}
+    {
+        $this->route = $route;
+        $this->path = $route['controller'] . '/' . $route['action'];
+    }
+    
+    public function view($tmpl, $vars = [], $layout) 
+    {
+        extract($vars);
+        $file = 'App/View/public/' . $this->path . '.php';
+        if (file_exists($file)) {
+            ob_start();
+            require $file;
+            $content = ob_get_clean();
+            require 'App/View/public/layouts/' . $layout . '.php';
+        }
+    }
 }
 
