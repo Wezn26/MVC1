294010fa9780d644db257ff7197b965bc4a00f40
Leonid Second Comp <li_che_in@mail.ru>
2023-02-13 13:49:00 +0300

    Add public functions getDataInClasses(), query(), getLastId() in App\Resources\Db.php

--- App/Resources/Db.php
+++ App/Resources/Db.php
@@ -14,5 +14,28 @@ class Db
         require __DIR__ . '/config.php';
         $this->dbh = new PDO(DSN);
     }
+    
+    public function query(string $sql, array $data = []) 
+    {
+        $stmt = $this->dbh->prepare($sql);
+        if (!empty($data)) {
+            foreach ($data as $key => $value) {
+                $stmt->bindValue(':' . $key, $value);
+            }
+        }
+        $stmt->execute();
+        return $stmt;
+    }
+    
+    public function getDataInClasses(string $sql, array $data = [], $class) 
+    {
+        $result = $this->query($sql, $data);
+        return $result->fetchAll(PDO::FETCH_CLASS, $class);
+    }
+    
+    public function getLastId() 
+    {
+        return $this->dbh->lastInsertId();
+    }
 }
 
