673957fd6eb8538d872c76d3e02c544d7f03cc65
Leonid Second Comp <li_che_in@mail.ru>
2023-02-17 13:40:19 +0300

    Create Article.php App\Models and add code

---
+++ App/Models/Article.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+
+class Article extends Model
+{
+    public $title;
+    public $image;
+    public $content;
+    public $author;
+    public $date;
+    
+    /*
+     * Функция возвращает $num последних записей из таблицы
+     * @param $num $integer
+     * @return array
+     * */
+    public static function findLastArticle(int $num) : array 
+    {
+        return array_reverse(array_slice(static::findAll(), -$num));
+    }
+}
+
